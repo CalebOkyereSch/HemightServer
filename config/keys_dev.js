ce76cc09677b77302a425bdb35129094ecdacbe4
KojoChamp <calebokyere1@gmail.com>
2020-07-27 04:03:54 +0000

    recent update v1.0

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://Caleb:12345@cluster0-2n1bt.mongodb.net/test?retryWrites=true&w=majority",
+  // mongoURI:"mongodb+srv://Caleb:12345@cluster0-2n1bt.mongodb.net/test?retryWrites=true&w=majority",\
+  mongoURI: "mongodb://127.0.0.1:27017",
   secretOrKey: "secrete",
 };
