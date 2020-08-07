be94434e9993d727227f3cb5908d2c2aa1465477
KojoChamp <calebokyere1@gmail.com>
2020-08-07 01:22:55 +0000

    changes made in mongoDB

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // mongoURI:
-  //    "mongodb+srv://Caleb:12345@cluster0-2n1bt.mongodb.net/test?retryWrites=true&w=majority",
-  mongoURI: "mongodb://127.0.0.1:27017",
+  mongoURI:
+    "mongodb+srv://Caleb:12345@cluster0-2n1bt.mongodb.net/test?retryWrites=true&w=majority",
+  // mongoURI: "mongodb://127.0.0.1:27017",
   secretOrKey: "secrete",
 };
